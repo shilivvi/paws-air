1a20ec967e42cb7a4df46b61e56b1b92537c54e7
shilivvi <vanya.ivanov161@gmail.com>
2022-05-08 15:47:34 +0300

    Added lazy load

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,1 +1,2 @@
 import './components/form-validate.js';
+import './components/lazy-load.js'
---
+++ src/js/components/lazy-load.js
@@ -0,0 +1,5 @@
+import LazyLoad from "vanilla-lazyload";
+
+const lazyContent = new LazyLoad({
+  use_native: true,
+});
